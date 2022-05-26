e9ba62a3197c29f457ce461e115f0d7761d0db9a
Gaurav Kumar Sinha <gauravkumarsinhasinha@gmail.com>
2022-05-26 09:04:11 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react'
+import JSONDATA from './MockData'
+// import {Data} from './mock_data'
+export default function App() {
+    const [s_data, setS_data] = useState("")
+    const [flag, setFlag] = useState(true)
+    function handleSearch(val){
+        if(val !== ''){
+            setFlag(false)
+            let data = JSONDATA.filter(
+                (item) =>
+                  item.first_name.toLowerCase().indexOf(val.toLowerCase()) !== -1
+              )
+            setS_data(data);
+        }
+        else{
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+            setFlag(true)
+        }
+    }
+     
+    return (
+      <>
+        <input type="text" placeholder='Search Your Item Here....' id="search" onChange={(e) => handleSearch(e.target.value)}/>
+        {
+            (flag && JSONDATA.map((item) => (<li id={item.id}>{item.first_name}</li>))) || (s_data.map((item) => (<li id={item.id}>{item.first_name}</li>)))
+        }
+      </>
+    );
+  }
